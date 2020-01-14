13eb83b3fa50dbea529a6e0fb7c0cd7a35742c0c
CernohorskyVaclav <vaclav.cernohorsky@gmail.com>
2020-01-14 13:58:28 +0100

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Václav Černohorský on 14/01/2020.
+//  Copyright © 2020 Václav Černohorský. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
